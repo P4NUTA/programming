3f58c4b126f161be03a258aba42aff916d61ae30
Pavel <PaulEfimov2001@yandex.ru>
2019-10-21 16:01:16 +0300

    lab_2_2

---
+++ Лаба 2_2/lab_2_2/lab_2_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	double x, x1, x2, y;
+	cout << "x1 = "; cin >> x1;
+	cout << "x2 = "; cin >> x2;
+	cout << "\tx\tsin(x)\n";
+	x = x1;
+	do
+	{
+		y = sin(x);
+		cout << "\t" << x << "\t" << y << endl;
+		x = x + 0.01;
+	} while (x <= x2);
+	return 0;
+}
