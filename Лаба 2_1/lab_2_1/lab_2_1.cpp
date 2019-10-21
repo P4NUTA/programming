e009712470f75a36d9a919ab708dbf1e7ef5645a
Pavel <PaulEfimov2001@yandex.ru>
2019-10-21 15:14:48 +0300

    lab_2_2 final

---
+++ Лаба 2_1/lab_2_1/lab_2_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char op;
+	cout << "Сделай свой выбор, собери авто свой мечты: ";
+	cin >> op;
+	switch (op)
+	{
+	case 'S':
+		cout << "Радио играть должно\n";
+		cout << "Колеса круглые\n";
+	case 'V':
+		cout << "Кондиционер хочу\n";
+		cout << "Радио играть должно\n";
+		cout << "Мощный двигатель\n";
+	default:
+		break;
+	}
+
+}
