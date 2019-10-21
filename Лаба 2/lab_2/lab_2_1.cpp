25593ef4a5f4c30ca939d3f894fd1db2c6f910f7
Павел <PaulEfimov2001@yandex.ru>
2019-10-21 14:46:05 +0300

    lab_2_1 final

---
+++ Лаба 2/lab_2/lab_2_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	// 1.1 point 
+	int x, y;
+	cout << "Write x and y: ";
+	cin >> x >> y;
+
+	if (x * x + y * y < 9 && y>0) {
+		cout << "inside" << endl;
+	}
+	else if (x * x + y * y > 9 || y < 0) {
+		cout << "Outside" << endl;
+	}
+	else {
+		cout << "on side" << endl;
+	}
+
+	// 1.2 point
+	int year;
+	cout << "Write year: ";
+	cin >> year;
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}
+
+}
