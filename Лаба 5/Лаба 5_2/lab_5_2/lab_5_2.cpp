9e94c9b0ef466d0491e8145ccc0bcd6fd3fccf94
Павел <PaulEfimov2001@yandex.ru>
2019-10-22 14:55:36 +0300

    lab_5_2_2

---
+++ Лаба 5/Лаба 5_2/lab_5_2/lab_5_2.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+using namespace std;
+void show_array(const int Arr[], const int N);
+bool from_min(const int a, const int b);
+bool from_max(const int a, const int b);
+void bubble_sort(int Arr[], const int N, bool (*compare)(int a, int b));
+
+int main()
+{
+	//2 point
+	const int N = 10;
+	int my_choose = 0;
+	int A[N] = { 9,8,7,6,1,2,3,5,4,9 };
+	cout << "1. Сортировать по возрастанию\n";
+	cout << "2. Сортировать по убыванию\n";
+	cin >> my_choose;
+	cout << "Исходные данные: ";
+	show_array(A, N);
+
+	bool (*from_f[2])(int, int) = { from_min,from_max };
+	bubble_sort(A, N, (from_f[my_choose - 1]));
+
+
+	//switch (my_choose)
+	//{
+	//case 1: bubble_sort(A, N, from_min); break;
+	//case 2: bubble_sort(A, N, from_max); break;
+	//default: cout << "\rНеизвестная операция ";
+	//}
+
+	show_array(A, N);
+
+	return 0;
+}
+
+bool from_min(const int a, const int b)
+{
+	return a > b;
+}
+bool from_max(const int a, const int b)
+{
+	return a < b;
+}
+
+void bubble_sort(int Arr[], const int N, bool (*compare)(int a, int b))
+{
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = 0; j < N - 1; j++)
+		{
+			if ((*compare)(Arr[j], Arr[j + 1])) swap(Arr[j], Arr[j + 1]);
+		}
+	}
+}
+
+void show_array(const int Arr[], const int N)
+{
+	for (int i = 0; i < N; i++)
+		cout << Arr[i] << " ";
+	cout << "\n";
+}
