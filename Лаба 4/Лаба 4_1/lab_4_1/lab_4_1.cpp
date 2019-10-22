7fe7fe6d793fcde2e4a1f82b2e55153a1ad1cb5d
Павел <PaulEfimov2001@yandex.ru>
2019-10-22 13:54:32 +0300

    lab_4_1 f

--- Лаба 4/Лаба 4_1/lab_4_1/lab_4_1.cpp
+++ Лаба 4/Лаба 4_1/lab_4_1/lab_4_1.cpp
@@ -12,7 +12,7 @@ void fum_ptr(double k, double* x, double* y)
 	*x = *x + k;
 	*y = *y + k;
 }
-void fum_ref(double k, double &x, double &y)
+void fum_ref(double k, double& x, double& y)
 {
 	x = x + k;
 	y = y + k;
@@ -21,7 +21,27 @@ void print(double x, double y)
 {
 	cout << "x = " << x << "; y = " << y << endl;
 }
-int Myroot(double a, double b, double c, double &x1, double &x2);
+int Myroot(double a, double b, double c, double& x1, double& x2) {
+	//ax ^ 2 + bx + c;
+	double D;
+	D = b * b + 4 * a * c;
+	if (D > 0) {
+		x1 = (-b + sqrt(D)) / 2 * a;
+		x2 = (-b - sqrt(D)) / 2 * a;
+		cout << "1" << endl;
+		return 1;
+	}
+	else if (D == 0) {
+		x1 = (-b) / 2 * a;
+		x2 = x1;
+		cout << "0" << endl;
+		return 0;
+	}
+	else {
+		cout << "-1" << endl;
+	return -1;
+	}
+}
 
 int main()
 {
@@ -36,5 +56,15 @@ int main()
 	print(xv, yv);
 	fum_ref(k, xv, yv); // Передача в функцию параметра ссылки
 	print(xv, yv);
+
+	//1.2 point
+
+	double a, b, c;
+	double x1=0, x2=0;
+	cout << "Write a, b, c : ";
+	cin >> a >> b >> c;
+	Myroot(a, b, c, x1, x2);
+	cout << "x1 = " << x1 << "\tx2 = " << x2 << endl;
+
 	return 0;
 }
