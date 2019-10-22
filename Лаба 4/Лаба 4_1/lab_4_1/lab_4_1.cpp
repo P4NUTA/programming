5476e1023d23487322d6a5257ae252d2acb65dc9
Павел <PaulEfimov2001@yandex.ru>
2019-10-22 11:18:10 +0300

    lab_4_1

---
+++ Лаба 4/Лаба 4_1/lab_4_1/lab_4_1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void fum_value(double k, double x, double y)
+{
+	x = x + k;
+	y = y + k;
+}
+void fum_ptr(double k, double* x, double* y)
+{
+	*x = *x + k;
+	*y = *y + k;
+}
+void fum_ref(double k, double &x, double &y)
+{
+	x = x + k;
+	y = y + k;
+}
+void print(double x, double y)
+{
+	cout << "x = " << x << "; y = " << y << endl;
+}
+int Myroot(double a, double b, double c, double &x1, double &x2);
+
+int main()
+{
+	//1.1 point
+	double k = 2.5;
+	double xv = 10;
+	double yv = 10;
+	print(xv, yv);
+	fum_value(k, xv, yv); // Передача в функцию обычного параметра
+	print(xv, yv);
+	fum_ptr(k, &xv, &yv); // Передача в функцию параметра указателя
+	print(xv, yv);
+	fum_ref(k, xv, yv); // Передача в функцию параметра ссылки
+	print(xv, yv);
+	return 0;
+}
