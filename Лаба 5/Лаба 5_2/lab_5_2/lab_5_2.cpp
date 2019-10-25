d6fb9ea48170e4e378e150f07eae9e5ca0a1a5ee
Павел <PaulEfimov2001@yandex.ru>
2019-10-25 15:45:49 +0300

    wtf

--- Лаба 5/Лаба 5_2/lab_5_2/lab_5_2.cpp
+++ Лаба 5/Лаба 5_2/lab_5_2/lab_5_2.cpp
@@ -1,26 +1,48 @@
 ﻿#include <iostream>
+#include <time.h>
+#include <windows.h>
 
 using namespace std;
 void show_array(const int Arr[], const int N);
 bool from_min(const int a, const int b);
 bool from_max(const int a, const int b);
 void bubble_sort(int Arr[], const int N, bool (*compare)(int a, int b));
+void random_num(int Arr[], int N);
+void write_num(int Arr[], int N);
 
 int main()
 {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	srand(time(NULL));
 	//2 point
+	bool (*from_f[2])(int, int) = { from_min,from_max };
+	cout << "Введите размер массива" << endl;
+	int n;
+	cin >> n;
+	int* myArray = new int[n];
 	const int N = 10;
 	int my_choose = 0;
 	int A[N] = { 9,8,7,6,1,2,3,5,4,9 };
+
+	cout << "1. Ввести значения самому\n";
+	cout << "2. Рандомно\n";
+	cin >> my_choose;
+
+	switch (my_choose)
+	{
+	case 1: write_num(myArray, n); break;
+	case 2: random_num(myArray, n); break;
+	default: cout << "\rНеизвестная операция ";
+	}
+
 	cout << "1. Сортировать по возрастанию\n";
 	cout << "2. Сортировать по убыванию\n";
 	cin >> my_choose;
 	cout << "Исходные данные: ";
-	show_array(A, N);
+	show_array(myArray, n);
 
-	bool (*from_f[2])(int, int) = { from_min,from_max };
-	bubble_sort(A, N, (from_f[my_choose - 1]));
-
+	bubble_sort(myArray, n, (from_f[my_choose - 1]));
 
 	//switch (my_choose)
 	//{
@@ -29,8 +51,8 @@ int main()
 	//default: cout << "\rНеизвестная операция ";
 	//}
 
-	show_array(A, N);
-
+	show_array(myArray, n);
+	delete[] myArray;
 	return 0;
 }
 
@@ -59,4 +81,19 @@ void show_array(const int Arr[], const int N)
 	for (int i = 0; i < N; i++)
 		cout << Arr[i] << " ";
 	cout << "\n";
+}
+
+void random_num(int Arr[], int N) {
+	int i;
+	for (i = 0; i < N; i++) {
+		Arr[i] = rand() % 15;
+	}
+}
+
+void write_num(int Arr[], int N) {
+	int i;
+	for (i = 0; i < N; i++) {
+		cout << "Напишите число " << i << ": ";
+		cin >> Arr[i];
+	}
 }
