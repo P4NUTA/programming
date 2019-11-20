f6d95aea52f0dd5d132645811278442049f521e8
Pavel <PaulEfimov2001@yandex.ru>
2019-11-20 04:35:20 +0300

    night fixes

--- Лаба 8/lab_8_1/lab_8_1.cpp
+++ Лаба 8/lab_8_1/lab_8_1.cpp
@@ -8,39 +8,66 @@ using namespace std;
 class Student
 {
 public:
-	// Установка имени студента
+	/*Установка имени студента
+
+	@param string student_name - student's name
+	@return none
+	*/
 	void set_name(string student_name)
 	{
 		name = student_name;
 	}
-	// Получение имени студента
+	/* Получение имени студента
+
+	@param none
+	@return none
+	*/
 	string get_name()
 	{
 		return name;
 	}
-	// Установка фамилии студента
+	/* Установка фамилии студента
+
+	@param string student_last_name - student's lastname
+	@return none
+	*/
 	void set_last_name(string student_last_name)
 	{
 		last_name = student_last_name;
 	}
-	// Получение фамилии студента
+	/* Получение фамилии студента
+
+	@return string
+	*/
 	string get_last_name()
 	{
 		return last_name;
 	}
-	// Установка промежуточных оценок
+	/* Установка промежуточных оценок
+
+	@param int[] student_scores - student's scores
+	@return none
+	*/
 	void set_scores(int student_scores[])
 	{
 		for (int i = 0; i < 5; ++i) {
 			scores[i] = student_scores[i];
 		}
 	}
-	// Установка среднего балла
+	/* Установка среднего балла
+
+	@param double ball
+	@return none
+	*/
 	void set_average_score(double ball)
 	{
 		average_score = ball;
 	}
-	// Получение среднего балла
+	/* Получение среднего балла
+
+	@param none
+	@return none
+	*/
 	double get_average_score()
 	{
 		return average_score;
@@ -72,7 +99,7 @@ int main()
 	for (int i = 0; i < 5; ++i) {
 		cout << "Score " << i + 1 << ": ";
 		cin >> scores[i];
-		// суммирование
+		// суммирование оценок
 		sum += scores[i];
 	}
 	// Сохранение имени и фамилии в объект класса Student
@@ -83,6 +110,7 @@ int main()
 	double average_score = sum / 5.0;
 	// Сохранение среднего балла в объект класса Student
 	student01.set_average_score(average_score);
+	// Вывод данных на экран
 	cout << "Average ball for " << student01.get_name() << " "
 		<< student01.get_last_name() << " is "
 		<< student01.get_average_score() << endl;
