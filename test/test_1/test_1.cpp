c5e0ddce623e2c96c82f1c3a3d400856f627d43f
Павел <PaulEfimov2001@yandex.ru>
2019-11-22 11:36:41 +0300

    test_1

---
+++ test/test_1/test_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+struct employee {
+public:
+	employee(int a, int b) {
+		ID = a;
+		value = b;
+	}
+	employee() {
+		ID = 0;
+		value = 0;
+	}
+	void show() {
+		cout << "ID: " << ID << " Value: " << value << endl;
+	}
+private:
+	int ID;
+	float value;
+};
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	employee em1(1, 20);
+	employee em2;
+	int a, b;
+	cout << "Введите данные о сотрудниках" << endl;
+	//cout << "первый: ";
+	//cin >> em1.ID >> em1.value;	
+	cout << "третий: ";
+	cin >> a >> b;
+	employee em3(a,b);
+	em1.show();
+	em2.show();
+	em3.show();
+	return 0;
+}
