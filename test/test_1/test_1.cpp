f42fffdf60f2ad68de25ba97d05ea03b25114d92
Павел <PaulEfimov2001@yandex.ru>
2019-11-22 11:51:30 +0300

    + comments

--- test/test_1/test_1.cpp
+++ test/test_1/test_1.cpp
@@ -1,5 +1,5 @@
 ﻿/* Создать структуру employee
-	
+
 	int ID;
 	int Value;
 	ввести данные и вывести
@@ -10,36 +10,55 @@ using namespace std;
 
 struct employee {
 public:
+	/*
+	Конструктор
+
+	@param int a - employee's ID
+	@param int b - employee's value
+	*/
 	employee(int a, int b) {
 		ID = a;
 		value = b;
 	}
+	/*
+	Конструктор для заполнения значений нулями
+	*/
 	employee() {
 		ID = 0;
 		value = 0;
 	}
+	/*
+	Деструктор
+	Выводит значения переменных
+	*/
+	~employee() {
+		show();
+	}
+	/*
+	Вывод данных о сотруднике
+
+	@return none
+	*/
 	void show() {
 		cout << "ID: " << ID << " Value: " << value << endl;
 	}
 private:
-	int ID;
-	float value;
+	int ID; // employee's ID
+	float value; // employee's value
 };
 int main()
 {
+	// Для русских символов
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+	// Объявление структур
 	employee em1(1, 20);
 	employee em2;
+	// Объявление переменных
 	int a, b;
 	cout << "Введите данные о сотрудниках" << endl;
-	//cout << "первый: ";
-	//cin >> em1.ID >> em1.value;	
 	cout << "третий: ";
 	cin >> a >> b;
-	employee em3(a,b);
-	em1.show();
-	em2.show();
-	em3.show();
+	employee em3(a, b);
 	return 0;
 }
