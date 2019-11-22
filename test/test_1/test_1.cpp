5ecfcdd824a9c05c26bd3870eff160f311b47528
Павел <PaulEfimov2001@yandex.ru>
2019-11-22 12:48:11 +0300

    test_1 final

--- test/test_1/test_1.cpp
+++ test/test_1/test_1.cpp
@@ -2,9 +2,10 @@
 #include <Windows.h>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
-struct employee {
+class employee {
 public:
 	/*
 	Конструктор
@@ -38,8 +39,52 @@ public:
 	void show() {
 		cout << "ID: " << ID << " Value: " << value << endl;
 	}
+	/*
+	Запись данных сотрудников в файл
+
+	@param ofstream& lout - file to write
+	@return none
+	*/
 	void filewrite(ofstream& lout) {
 		lout << "ID: " << ID << " Value: " << value << endl;
+	}
+	/*
+	Изменение данных сотрудника
+	
+	@param none
+	@return none
+	*/
+	void change() {
+		int a;
+		float b;
+		int mychoose;
+		cout << "1. Изменить ID" << endl;
+		cout << "2. Изменить зарплату" << endl;
+		cout << "3. Изменить все данные" << endl;
+		cin >> mychoose;
+		switch (mychoose) {
+		case 1:
+			cout << "ID: ";
+			cin >> a;
+			ID = a;
+			break;
+		case 2:
+			cout << "Value: ";
+			cin >> b;
+			value = b;
+			break;
+		case 3:
+			cout << "ID: ";
+			cin >> a;
+			cout << "Value: ";
+			cin >> b;
+			ID = a;
+			value = b;
+			break;
+		default:
+			cout << "Ошибка" << endl;
+		}
+
 	}
 private:
 	int ID; // employee's ID
@@ -51,20 +96,29 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	// Объявление структур
-	employee em1;
-	employee em2(1,20);
+	employee em1(0, 10);
+	employee em2(1, 20);
 	// Объявление переменных
-	int a, b;
+	int a;
+	float b;
+	// Ввод данных сотрудника при создании
 	cout << "Введите данные о сотрудниках" << endl;
-	cout << "третий: ";
-	cin >> a >> b;
+	cout << "третий: " << endl;
+	cout << "ID: ";
+	cin >> a;
+	cout << "Value: ";
+	cin >> b;
 	employee em3(a, b);
+	// Ввод данных сотрудника при изменении
+	em1.change();
 	// Открыть файл для записи данных сотрудников
 	// Открытие файла
 	ofstream file_("text.txt");
+	// Проверка открылся ли файл
 	if (!file_) {
 		cout << "error" << endl;
 	}
+	// Запись файлов
 	em1.filewrite(file_);
 	em2.filewrite(file_);
 	em3.filewrite(file_);
