de415e3f3439549884e160c7da05cae8b86a82e0
Pavel <PaulEfimov2001@yandex.ru>
2019-12-18 18:59:39 +0300

    report changes

--- Лаба 11/lab_11_1/lab_11_1.cpp
+++ Лаба 11/lab_11_1/lab_11_1.cpp
@@ -14,8 +14,7 @@ public:
 	Distance() : feet(0), inches(0.0), MTF(3.280833F) { }
 	// конструктор с двумя параметрами
 	Distance(int ft, float in) : feet(ft), inches(in), MTF(3.280833F) { }
-	Distance(float meters) : MTF(3.280833F)
-	{
+	Distance(float meters) : MTF(3.280833F) {
 		float fltfeet = MTF * meters; // перевод в футы
 		feet = int(fltfeet); // число полных футов
 		inches = 12 * (fltfeet - feet); // остаток - это дюймы
@@ -31,9 +30,8 @@ public:
 	{
 		cout << feet << "\' - " << inches << "\"\n";
 	}
-	Distance operator+ (Distance) const;
-	Distance operator- (Distance) const;
-
+	//Distance operator+ (Distance) const;
+	//Distance operator- (Distance) const;
 	operator float() const
 	{
 		float fracfeet = inches / 12;
@@ -75,31 +73,38 @@ int main()
 	float mtrs;
 	mtrs = static_cast<float>(dist5);
 	mtrs = dist2;
-
 	cout << mtrs;
 	return 0;
 }
 
-Distance Distance::operator+ (Distance d2) const
-{
-	int f = feet + d2.feet;
-	float i = inches + d2.inches;
-	if (i >= 12.0)
-	{
-		i -= 12.0;
-		f++;
-	}
-	return Distance(f, i);
-}
-Distance Distance::operator- (Distance d2) const
-{
-	int f = feet - d2.feet;
-	float i = inches - d2.inches;
-	if (i >= 12.0)
-	{
-		i -= 12.0;
-		f++;
-	}
-	return Distance(f, i);
-	
-}
+//Distance Distance::operator+ (Distance d2) const
+//{
+//	int f = feet + d2.feet;
+//	float i = inches + d2.inches;
+//	if (i >= 12.0)
+//	{
+//		i -= 12.0;
+//		f++;
+//	}
+//	return Distance(f, i);
+//}
+//Distance Distance::operator- (Distance d2) const
+//{
+//	int f;
+//	float i;
+//	if ((feet >= d2.feet && inches >= d2.inches) || (feet > d2.feet)) {
+//		f = feet - d2.feet;
+//		i = inches - d2.inches;
+//		if (i < 0) {
+//			i += 12.0;
+//			f--;
+//		}
+//		else if (i >= 12.0)
+//		{
+//			i -= 12.0;
+//			f++;
+//
+//		}
+//	return Distance(f, i);
+//	}
+//}
