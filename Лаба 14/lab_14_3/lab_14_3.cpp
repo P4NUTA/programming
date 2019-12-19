e559645afea44d687bbe1b6b947fb6e2ebf2496e
Pavel <PaulEfimov2001@yandex.ru>
2019-12-19 17:16:09 +0300

    lab_14

--- Лаба 14/lab_14_3/lab_14_3.cpp
+++ Лаба 14/lab_14_3/lab_14_3.cpp
@@ -2,26 +2,32 @@
 
 using namespace std;
 
-template<class T, class T1> class array
+template<class T, class T1> class Array
 {
 public:
-	array(int size);
+	Array(int size);
 	T1 sum();
 	T average_value();
-	void show_array();
+	void show_Array();
 	int add_value(T);
 private:
 	T* data;
 	int size;
 	int index;
 };
-template<class T, class T1> T array<T, T1>::average_value()
+/*Среднее арифметическое
+@return T1 - среднее арифметическое массива
+*/
+template<class T, class T1> T Array<T, T1>::average_value()
 {
 	T1 sum = 0;
 	for (int i = 0; i < index; i++) sum += data[i];
 	return (sum / index);
 }
-template<class T, class T1> array<T, T1>::array(int size)
+/*Конструктор массива
+@param int size - размер массива
+*/
+template<class T, class T1> Array<T, T1>::Array(int size)
 {
 	data = new T[size];
 	if (data == NULL)
@@ -29,27 +35,30 @@ template<class T, class T1> array<T, T1>::array(int size)
 		cerr << "Error memory ---- exit program" << endl;
 		exit(1);
 	}
-	array::size = size;
-	array::index = 0;
+	Array::size = size;
+	Array::index = 0;
 }
-template<class T, class T1> T1 array<T, T1>::sum()
+/*Сумма массива
+@param - none
+@return T1 sum - сумма всех элементов массива
+*/
+template<class T, class T1> T1 Array<T, T1>::sum()
 {
 	T1 sum = 0;
 	for (int i = 0; i < index; i++) sum += data[i];
 	return(sum);
 }
-template<class T, class T1> T array<T, T1>::average_value()
-{
-	T1 sum = 0;
-	for (int i = 0; i < index; i++) sum += data[i];
-	return (sum / index);
-}
-template<class T, class T1> void array<T, T1>::show_array()
+/*Вывод массива на экран
+*/
+template<class T, class T1> void Array<T, T1>::show_Array()
 {
 	for (int i = 0; i < index; i++) cout << data[i] << ' ';
 	cout << endl;
 }
-template<class T, class T1> int array<T, T1>::add_value(T value)
+/*Ввод значений в массив
+@param T value - значение
+*/
+template<class T, class T1> int Array<T, T1>::add_value(T value)
 {
 	if (index == size)
 		return(-1);
@@ -62,26 +71,18 @@ template<class T, class T1> int array<T, T1>::add_value(T value)
 }
 int main()
 {
-	array<int, long> numbers(100);
-	array<float, float> values(200);
+	// Объявление массивов 
+	Array<int, long> numbers(100);
+	Array<float, float> values(200);
 	int i;
+	// Работа с первым массивом
 	for (i = 0; i < 50; i++) numbers.add_value(i);
-	numbers.show_array();
+	numbers.show_Array();
 	cout << "Sum = " << numbers.sum() << endl;
 	cout << "Average = " << numbers.average_value() << endl;
+	// Работа со вторым массивом
 	for (i = 0; i < 100; i++) values.add_value(i * 100);
-	values.show_array();
+	values.show_Array();
 	cout << "Sum = " << values.sum() << endl;
 	cout << "Average = " << values.average_value() << endl;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
